793301b3dc29cce5610b7f4e6e2d1af77fc76d28
20398 <parth1403.cs@gmail.com>
2023-08-16 08:00:09 -0400

    floder structure edited

--- template.py
+++ template.py
@@ -17,11 +17,11 @@ list_of_files = [
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/constants/__init__.py"
-    "config/config.yaml"
-    "dvc.yaml"
-    "parms.yaml"
-    "recuirments.txt"
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "parms.yaml",
+    "recuirments.txt",
     "setup.py",
     "research/trails.ipynb"   
 ]
@@ -32,7 +32,7 @@ for filepath in list_of_files:
     
     if filedir != "":
         os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory;{filedir} for the file:{filename}")
+        logging.info(f"Creating directory;{filedir} for the file:{filename}") 
     
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,"w") as f:
